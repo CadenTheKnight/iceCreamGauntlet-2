0e0b26cd8c8718a907375a5ab7f9949cd867eced
CadenTheKnight <111906442+CadenTheKnight@users.noreply.github.com>
2023-02-01 17:09:18 -0500

    2-1 Update. Adding a working enemy sprite and its enemyController script. To implement into the main document, follow the notes I post in the google doc.

---
+++ Assets/scripts/enemyController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    private GameObject player;
+    private Vector2 playerLocation;
+
+    //public variables-------------------
+    public float speed = 0.2f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Object.FindObjectOfType<playerController>().gameObject;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLocation = player.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, playerLocation, speed * Time.deltaTime);
+
+        //movement to rigidbody to prevent jittering collisions
+        rigidbody2d.MovePosition(transform.position);
+
+        /* ------- this code didn't work but has potential to reduce enemy jittering ---------
+        playerLocation = player.transform.position;
+        Vector2 newPosition = transform.position;
+        newPosition = Vector2.MoveTowards(newPosition, playerLocation, speed * Time.deltaTime);
+
+        movement to rigidbody to prevent jittering collisions
+        rigidbody2d.MovePosition(newPosition);
+        */
+
+        //rigidbody2d.MovePosition(Vector2.MoveTowards(transform.position, playerLocation, speed * Time.deltaTime));
+    }
+
+
+
+}
--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -8,6 +8,13 @@ public class playerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vert;
+    
+    bool canBeDamaged = true;
+    public float timeInvincible = 1.5f;
+    float invincibleTimer;
+
+    public int maxHealth = 10;
+    int health;
 
     //public variables-------------------
     public float speed = 0.2f;
@@ -16,6 +23,7 @@ public class playerController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        health = maxHealth;
     }
 
     // Update is called once per frame
@@ -24,6 +32,16 @@ public class playerController : MonoBehaviour
         //movement axes
         horizontal = Input.GetAxis("Horizontal");
         vert = Input.GetAxis("Vertical");
+
+        //if you can be damaged (not invincible), then count down invincibility timer
+        if(!canBeDamaged)
+        {
+            invincibleTimer -= timeInvincible * Time.deltaTime;
+            if(invincibleTimer < 0)
+            {
+                canBeDamaged = true;
+            }
+        }
        
     }
 
@@ -35,5 +53,28 @@ public class playerController : MonoBehaviour
         position.y = position.y + speed * vert * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+
+        //if you collide with an enemy, start timer, decrease health, and check for game over (if health < 0)
+        if(col.gameObject.tag == "enemy" && canBeDamaged)
+        {
+            Debug.Log("ENEMY HIT YOU");
+            canBeDamaged = false;
+            invincibleTimer = timeInvincible;
+            health--;
+            if(health <= 0)
+            {
+                GameOver();
+            }
+        }
+        
+    }
+    
+    void GameOver()
+    {
+        Debug.Log("Game Over. Health: " + health);
     }
 }
